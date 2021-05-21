76123ddd48420652d1a03596145d4838de1ffd66
Kingsley Twum <61196203+mclegacycodes@users.noreply.github.com>
2021-05-21 13:16:13 +0200

    finished with v1

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var keys = document.querySelectorAll("button");
 var actions = document.querySelectorAll("button[data-action]");
 var display = document.getElementById("display");
+var answer=document.getElementById('answer');
 
 //function to display number when key is pressed
 keys.forEach(key => key.addEventListener('click',function(e){
@@ -11,10 +12,24 @@ keys.forEach(key => key.addEventListener('click',function(e){
     
 if(key.innerText=="DEL"){
     display.value="";
-}else{
-    //console.log(e.target.dataset.action);
-    display.value+= key.innerText;
+}else if(e.target.dataset["action"]){
+    display.value+= e.target.dataset.action;
+ 
+}
+else if(e.target.dataset["value"]){
+    display.value+= e.target.dataset.value;
+}
+else{
+        answer.innerText=""
+   answer.innerText= eval(display.value);
 }
     
 
-}))
+}));
+
+//returning the calculation when the equal sign in clicked
+// var equals=document.getElementById("equalsign");
+// equals.addEventListener('click',function(e){
+//     answer.innerText=""
+//     answer.innerText= eval(display.value);
+// })
